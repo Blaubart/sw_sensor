e8d4eec91839047456aa6587a407b6afee1109bb
realtimepeople <klaus.schaefer@h-da.de>
2026-03-28 12:54:29 +0100

    More information about config changes reported.

--- sw_stm32/Communication/CAN_listener.cpp
+++ sw_stm32/Communication/CAN_listener.cpp
@@ -86,7 +86,7 @@ bool EEPROM_config_read_write( const CANpacket & p, float & return_value)
 
 	bool success = write_EEPROM_value( id, value);
 	communicator_command_queue.send( SOME_EEPROM_VALUE_HAS_CHANGED, 1);
-	signal_logger_event( EEPROM_CONFIGURATION_CHANGED | (success ? 0x100 : 0) );
+	signal_logger_event( EEPROM_CONFIGURATION_CHANGED | (success ? id + 0x100 : id) );
 	return false; // report "nothing read"
       }
       break;
